c34844830b201930ea030c2e5ddc8603cc312180
Raphael DUCOM <raphael.ducom@gmail.com>
2015-09-28 20:10:51 +0200

    add ConfigOverwrite test

---
+++ StackCache.Test/ConfigOverwrite.cs
@@ -0,0 +1,60 @@
+﻿namespace StackCache.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Core.Configuration;
+    using Xunit;
+
+    public class ConfigOverwrite
+    {
+        private readonly SettingsManager _settingsManager;
+
+        public ConfigOverwrite()
+        {
+            this._settingsManager = SettingsManager.NewSettingsManager();
+        }
+
+        [Fact]
+        public async Task TestReloadSettings()
+        {
+            // test in \Debug\bin\
+            this._settingsManager.ConfigFolder = AppDomain.CurrentDomain.BaseDirectory;
+            var rs = new RedisSetting
+            {
+                ServerName = "localhost",
+                RedisInstances = new List<RedisServer>
+                {
+                    new RedisServer {Hostname = "127.0.0.1", Port = 6379}
+                }
+            };
+            this._settingsManager.SaveConfigFiles(new Setting[] {rs});
+
+            await Task.Delay(100);
+
+            var found = this._settingsManager.GetSetting<RedisSetting>();
+
+            Setting[] testSettings =
+            {
+                new RedisSetting
+                {
+                    ServerName = "localhost_test_update",
+                    RedisInstances = new List<RedisServer>
+                    {
+                        new RedisServer {Hostname = "127.0.0.1", Port = 6379}
+                    }
+                }
+            };
+
+            var settingChanged = false;
+            found.SettingFileChanged += (s, e) => { settingChanged = true; };
+
+            this._settingsManager.SaveConfigFiles(testSettings);
+
+            await Task.Delay(100);
+
+            Assert.True(settingChanged);
+            Assert.True(found.ServerName == "localhost_test_update");
+        }
+    }
+}
