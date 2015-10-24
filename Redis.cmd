7d2f51dc2471b8637276f28cc4c5cc0409e12af9
Raphael DUCOM <raphael.ducom@gmail.com>
2015-10-24 14:33:38 +0200

    Update redis cmd to start as non-service instance Added kill command

--- Redis.cmd
+++ Redis.cmd
@@ -1,3 +1,2 @@
 .nuget\nuget.exe install redis-64 -excludeversion
-redis-64\redis-server.exe --service-install 
-redis-64\redis-server.exe --service-start
+START /B redis-64\redis-server.exe --maxheap 512M  > redislog.txt
