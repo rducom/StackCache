72bccea4428ef56036028d20074d26584ae3f373
Raphael DUCOM <raphael.ducom@gmail.com>
2015-10-25 19:43:07 +0100

    fix sonar batch

--- SonarRunner.cmd
+++ SonarRunner.cmd
@@ -1,5 +1,6 @@
-IF "%APPVEYOR_PULL_REQUEST_NUMBER%"=="" 
-(
+@echo off
+
+IF "%APPVEYOR_PULL_REQUEST_NUMBER%"=="" (
 MSBuild.SonarQube.Runner.exe begin /k:StackCache /n:StackCache /v:1.0 ^
 	/d:sonar.host.url=%SonarServer% ^
 	/d:sonar.login=%SonarUser% ^
@@ -7,9 +8,7 @@ MSBuild.SonarQube.Runner.exe begin /k:StackCache /n:StackCache /v:1.0 ^
 	/d:sonar.github.repository=%APPVEYOR_REPO_NAME% ^
 	/d:sonar.github.login=StackCacheOps ^
 	/d:sonar.github.oauth=753dc16f860f7b1581cdd8b9b9b77596270df73e 
-)
-ELSE
-(
+) ELSE (
 MSBuild.SonarQube.Runner.exe begin /k:StackCache /n:StackCache /v:1.0 ^
 	/d:sonar.host.url=%SonarServer% ^
 	/d:sonar.login=%SonarUser% ^
